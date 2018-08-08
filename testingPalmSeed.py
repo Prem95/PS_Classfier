c069d0e8f489d8dee948178f7bdaeb58b8a604d3
Prem95 <premstroke95@gmail.com>
2018-08-09 00:52:54 +0800

    .

--- testingPalmSeed.py
+++ testingPalmSeed.py
@@ -3,10 +3,17 @@ import tensorflow as tf
 import os
 import cv2
 from matplotlib import pyplot as plt
+import shutil
+from alexnetType1 import AlexNet
+from caffe_classes import class_names
+
+# Initializer
+num_classes = 2
+original_class_label = ''
 
 imagenet_mean = np.array([104., 117., 124.], dtype=np.float32)
 
-# Import the test files
+# //* Import the test files
 test_path = './Test_Bank1.txt' 
 test_path_imagesAppend = os.path.join(test_path, 'images')
 testImagePathList = []
@@ -17,6 +24,7 @@ with open(test_path, 'r') as tp:
 
     testImages = []
     labels = []
+
     #! Basic enumerate and split and store in list. Very basic for files searching.
     for index, files in enumerate(tp):
 
@@ -25,6 +33,20 @@ with open(test_path, 'r') as tp:
         testLabelsPathList.append(label)
         testImages.append(cv2.imread(img))
         labels.append(label)
+print("Total number of files = " + str(len(testImagePathList)))
+
+resultsFilename = './TestingResults/Result_Classifier_1.txt'
+
+# Check file path
+if (os.path.isdir('MISCLASSIFIED')) == True:
+    print('MISSCLASSIFIED -  Exists')
+else:
+    os.makedirs('MISCLASSIFIED')
+
+if (os.path.isdir('CORRECTCLASSIFIED')) == True:
+    print('CORRECTCLASSIFIED - Exists')
+else:
+    os.makedirs('CORRECTCLASSIFIED')
 
 
 
